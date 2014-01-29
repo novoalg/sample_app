519b12c6b2a1fa20ff6b6d6715602cdc016e7834
novoalg <novoalg@uwec.edu>
2014-01-29 16:51:25 -0600

    Changed my Routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,18 @@ class StaticPagesController < ApplicationController
   def home
   end
 
+  def signin 
+  end
+
+  def signup
+  end
   def help
   end
  
   def about
   end
+
+  def contact
+  end
+
 end
