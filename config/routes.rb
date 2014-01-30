ce9d487112b50a281e93725edf7367395f6d4a0a
novoalg <novoalg@uwec.edu>
2014-01-30 12:35:39 -0600

    Finished chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 SampleApp::Application.routes.draw do
  
+  get "users/new"
+
 	match '/', :to => 'static_pages#home'
  	match '/about', :to => 'static_pages#about'
  	match '/help', :to => 'static_pages#help'
  	match '/contact', :to => 'static_pages#contact'
- 	match '/signup', :to => 'static_pages#signup'
+ 	match '/signup', :to => 'users#new'
  	match '/signin', :to => 'static_pages#signin'
           
 	root :to => 'static_pages#home'
